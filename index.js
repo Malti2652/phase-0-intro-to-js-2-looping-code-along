8124ccbdfeb42d354f011084699d4ba1effd9ba7
Eddington Rowellis <eddingtonibi@gmail.com>
2022-12-25 01:22:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+const count = [10,9,8,7,6,5,4,3,2,1,];
+
+function countDown() {
+  let i = 0;
+  while (i < 10) {
+    console.log(count[i]);
+    i++;
+  }
+
+  return count;
+
+}
+
+countDown(10);
